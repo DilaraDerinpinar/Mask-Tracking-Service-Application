c0a1361196364cb47ca83a252cd1f730aecb68b2
Dilara Derinpinar <dilaraderinpinar1@gmail.com>
2022-04-17 01:27:50 +0300

    Add project files.

---
+++ WorkAround/Program.cs
@@ -0,0 +1,33 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace WorkAround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vatandas vatandas = new Vatandas();
+            Person person1 = new Person();
+
+            person1.FirstName = "engin";
+            person1.LastName = "demiroğ";
+            person1.DateOfBirthYear = 1985;
+            person1.NationalIdentity = 123;
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+        }
+    }
+
+    public class Vatandas
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DoğumYili { get; set; }
+        public long TcNo { get; set; }
+    }
+
+}
